d62b930862508d0b19619e53f24bcc0be40f6225
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-05 17:03:59 -0300

    feat: Add validation and properties to CreateCuotaDto
    This commit adds validation decorators and properties to the CreateCuotaDto class. The new properties include title, value, paid, qty, card, and createdAt. These changes enhance the data model for creating cuotas and ensure that the required fields are not empty. This improvement improves the reliability and usability of the CreateCuotaDto class.

--- src/cuotas/dto/create-cuota.dto.ts
+++ src/cuotas/dto/create-cuota.dto.ts
@@ -1,1 +1,26 @@
-export class CreateCuotaDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateCuotaDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  value: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  paid: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  qty: number;
+
+  @IsString()
+  @IsNotEmpty()
+  card: string;
+
+  @IsOptional()
+  createdAt: Date;
+}
