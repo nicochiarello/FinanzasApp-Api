3f06b5291c5109e4029a14e25529bf8a4a218712
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-11 12:22:04 -0300

    Updated main.ts to use ConfigService for obtaining the PORT variable and set a global prefix for the API

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8080);
+
+  const configService = app.get(ConfigService); // Obtener una instancia de ConfigService
+  const PORT = configService.get<number>('PORT') || 8080; // Obtener la variable PORT desde ConfigService
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(PORT);
+  Logger.log(`🚀 Server running on port ${PORT}`, 'Bootstrap');
 }
 bootstrap();
